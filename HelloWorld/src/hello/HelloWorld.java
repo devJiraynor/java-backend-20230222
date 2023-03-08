61431f9ab57487a1ea14575820d8bd2d9d447868
devJiraynor <jiraynor@naver.com>
2023-03-08 12:39:54 +0900

    2023.03.08

--- HelloWorld/src/hello/HelloWorld.java
+++ HelloWorld/src/hello/HelloWorld.java
@@ -1,10 +1,25 @@
 package hello;
 
+class Example {
+	String destination;
+	
+	Example() {
+		destination = "서면";
+	}
+	
+	Example(String destination) {
+		this.destination = destination;
+	}
+}
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello Java");
+		
+		Example example = new Example();
+		example.destination = "부산역";
 	}
 
 }
